578b47e9ea85a864bb172a5c4ff4e60d3b4a1c8a
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-04 13:17:03 +0530

    Create fullname.js
    positive case:
    guvi
    geeks
    guvigeeks
    negative case:
    guvi
    geeks
    guvi geeks

---
+++ JS/home/fullname.js
@@ -0,0 +1,7 @@
+function getFullName(firstName,lastName) {
+  
+  console.log(firstName+lastName);
+};
+firstName=prompt("enter your first name:");
+lastName=prompt("enter your last name:")
+getFullName(firstName,lastName)
