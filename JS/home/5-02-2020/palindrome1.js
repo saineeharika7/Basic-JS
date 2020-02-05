5dfe52d10309c6ed4a1160d517ea64ce44d51607
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-05 20:40:37 +0530

    Create palindrome1.js

---
+++ JS/home/5-02-2020/palindrome1.js
@@ -0,0 +1,12 @@
+(function() {
+var  b=['121','51','676','7777','83'];
+for(var i=0;i<b.length;i++) {
+  var d=b[i].length;
+  if(b[i][0]==b[i][d-1]){
+    console.log(b[i]);
+  }
+    
+  }
+     
+})
+  ();
