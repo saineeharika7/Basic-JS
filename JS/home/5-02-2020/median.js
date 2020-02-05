72e55caf1c903d677cd1fecc0b34ce8b1c3584d3
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-05 20:22:35 +0530

    Create median.js

---
+++ JS/home/5-02-2020/median.js
@@ -0,0 +1,11 @@
+var a=function() {
+ var b=[1,2,7,9,11];
+ var c=[2,4,7,8,10];
+ var d=b.sort(function(z,y){return z - y});
+ var e=c.sort(function(z, y){return z - y});
+ var f=Math.floor((d.length)/2);
+ var g=(d[f]+e[f]);
+ var h=g/2;
+ console.log(h);
+};
+  a();
