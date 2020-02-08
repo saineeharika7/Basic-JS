6bc69c5cbb2a0232af6aa6ff3d75a3df35040728
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-08 12:44:09 +0530

    Create calcu.js

---
+++ JS/home/calcu.js
@@ -0,0 +1,17 @@
+var funn= function(a,b) {
+var a = prompt("enter your choice");
+  if(a==1) {
+    console.log(a+b);
+  };
+  if(a==2) {
+    console.log(b-a);
+  };
+  if(a==3) {
+    console.log(a*b);
+  };
+  if(a==4) {
+    console.log(b/a);
+  };
+};
+
+funn(2,5);
