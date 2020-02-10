a7bd26ecd7ad7dba3e9907d1458d7a1fd67a9fd6
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-10 16:27:05 +0530

    Update overs.js

--- jsclass/overs.js
+++ jsclass/overs.js
@@ -6,7 +6,4 @@ class overs{
 };
 var ovr1 = new overs(10,2);
 var ovr2 = new overs(20,0);
-console.log(ovr1.score);
-console.log(ovr1.wickets);
-console.log(ovr2.score);
-console.log(ovr2.wickets);
+
