b83786bda0196f304099fc09f2306982a55be045
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-10 19:05:02 +0530

    Create team.js

---
+++ jsclass/team.js
@@ -0,0 +1,15 @@
+
+class teams{
+  constructor(teamname){
+    this.teamname = teamname;
+  }
+    players(){
+    var player = new array();
+    for(i =0; i<11; i++){
+    var player_name = prompt();
+    player.push(player_name);
+  }
+     return player;
+}
+
+}
