2e6aeed70962eab8c4eb35e9736f7b35e04be898
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-10 16:53:16 +0530

    Create players.js

---
+++ jsclass/players.js
@@ -0,0 +1,12 @@
+class player{
+  constructor(name){ 
+    this.name = name;
+}
+score(){
+  var playerscore = prompt("score:")
+  return playerscore;
+}
+ballsplayed(){
+  var balls = prompt("balls:")
+  return balls;
+}
