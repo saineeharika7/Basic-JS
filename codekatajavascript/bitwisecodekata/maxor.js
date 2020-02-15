2703c746bcbc64f69d533b2c388e20f4de795b70
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-15 14:34:20 +0530

    Create maxor.js
    INPUT
    2
    2 4
    OUTPUT
    6

---
+++ codekatajavascript/bitwisecodekata/maxor.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+    input:process.stdin
+});
+const userInput = [];
+inp.on("line",(data) => {
+    userInput.push(data);
+});
+inp.on("close",() => {
+    console.log(fun(userInput));
+});
+function fun(data){
+    var a = parseInt(data[0]);
+    var b = data[1].split(' ');
+    
+    var or = Math.max((b[0]|b[1]));
+    
+    
+    return or
+}
