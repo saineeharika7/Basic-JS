99ef8cc9a39584a058309f7526ffd934abe4b7ec
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-15 13:42:45 +0530

    Create rightshift.js
    INPUT
    5 2
    OUTPUT
    1

---
+++ codekatajavascript/bitwisecodekata/rightshift.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+    input:process.stdin
+});
+const userInput = [];
+inp.on("line",(data) => {
+    userInput.push(data);
+});
+inp.on("close",() => {
+    console.log(fun(userInput));
+});
+function fun(data){
+    var array = data[0].split(' ');
+    var a = parseInt(array[0]);
+    var b = parseInt(array[1]);
+    var c =a>>b;
+    return c
+    
+}
