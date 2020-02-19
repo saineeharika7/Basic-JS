5cd2b8cf9cff708385753cecce4f9ad9e29470b5
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-19 18:59:13 +0530

    Create usingrange.js

---
+++ jspractise/usingrange.js
@@ -0,0 +1,18 @@
+<DOCTYPE.html>
+  <html>
+  <head>
+    <script>
+      function changevalue(){
+        var x = document.getElementById("a").style.color ="yellow";
+      }
+
+      </script>
+    </head>
+    <body>
+      <div id ="a">changing with style.color property</div>
+      <p>
+        <input type ="range" value="changevalue attribute" onchange="changevalue()">
+     </p>
+
+      <body>
+    </html>
