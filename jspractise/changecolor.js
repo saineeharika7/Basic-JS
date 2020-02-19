a6d0a0af73ca9bcd3023d5411146655b082bff8b
saineeharika7 <60165315+saineeharika7@users.noreply.github.com>
2020-02-19 18:20:21 +0530

    Create changecolor.js

---
+++ jspractise/changecolor.js
@@ -0,0 +1,17 @@
+<DOCTYPE.html>
+  <html>
+  <head>
+    <script>
+      function changevalue(){
+        var x = document.getElementById("a").style.color ="yellow";
+      }
+
+      </script>
+    </head>
+    <body>
+      <div id ="a">changing with style.color property</div>
+      <p>
+        <input type ="button" value="changevalue attribute" onclick="changevalue()">
+     </p>
+      <body>
+    </html>
